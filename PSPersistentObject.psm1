fe5427ca5459cffec3d7235a1e4f80de5b866267
Ludvig Ahlén <ludvig.ahlen@gmail.com>
2021-07-22 09:15:05 +0200

    Added stuff

--- PSPersistentObject.psm1
+++ PSPersistentObject.psm1
@@ -1,4 +1,187 @@
-function New-PSPersistentObject {
+try {
+    # Import sqlite assemblies
+
+    if([IntPtr]::size -eq 8) #64
+    {
+        $SQLiteAssembly = Join-path $PSScriptRoot "x64\System.Data.SQLite.dll"
+    }
+    elseif([IntPtr]::size -eq 4) #32
+    {
+        $SQLiteAssembly = Join-path $PSScriptRoot "x86\System.Data.SQLite.dll"
+    }
+
+    Add-Type -path $SQLiteAssembly -ErrorAction stop
+
+
+
+} catch {
+    Throw 'This module requires an SQLite driver'
+}
+
+$DBLocation = "$((Get-location).path)\PSPersistentObject.db"
+
+function New-DBConnection {
+
+    $ConnectionString = "Data source=$DBLocation"
+
+    $conn = New-Object System.Data.SQLite.SQLiteConnection -ArgumentList $ConnectionString
+    
+    $conn.ParseViaFramework = $true
+    
+        try {
+            $conn.Open() 
+        }
+        Catch {
+            Write-Error $_
+            continue
+        }
+ 
+
+    $Conn
+}
+
+function Close-DBConnection {
+
+    param($Connection)
+
+    try {
+    $Connection.Close()
+    $Connection.Dispose()
+
+    } catch {
+        Write-Error $_
+        continue
+
+    }
+
+
+}
+
+function Invoke-DBNonQuery {
+    param(
+        $connection,
+        $query
+        )
+
+        $connectionCreated = $false
+
+        if(-not $connection){
+            
+            $connection = New-DBConnection
+            $connectionCreated = $true
+        }
+        $command = $connection.createCommand()
+        $command.commandtext = $query
+
+
+     try {
+
+    $command.executeNonQuery()  
+
+            if($connectionCreated){
+                Close-DBConnection -Connection $connection
+            }
+            $command.Dispose()
+        }
+        Catch
+        { 
+            if($connectionCreated){
+                Close-DBConnection -Connection $connection
+                write-error $_
+                continue
+            }
+        }
+
+}
+
+function Invoke-DBQuery {
+    param(
+        $connection,
+        $Query
+        )
+
+        $connectionCreated = $false
+
+        if(-not $connection){
+            
+            $connection = New-DBConnection
+            $connectionCreated = $true
+        }
+        $command = $connection.createCommand()
+        $command.commandtext = $Query
+
+        $ds = New-Object system.Data.DataSet 
+        $da = New-Object System.Data.SQLite.SQLiteDataAdapter($command)
+
+        Try
+        {
+            [void]$da.fill($ds)
+         
+            $command.Dispose()
+        }
+        Catch
+        { 
+            if($connectionCreated){
+                Close-DBConnection -Connection $connection
+                write-error $_
+                continue
+            }
+        }
+
+        $objects = Convert-DataTable -datatable $ds
+        
+        if($connectionCreated){
+            if($connection.state -eq "Open"){
+            Close-DBConnection -Connection $connection
+            }
+        }
+      
+
+        $objects
+}
+
+function Convert-DataTable {
+    param($DataTable)
+
+    $psObjects = @()
+    $Rows = $DataTable.tables[0].rows
+    foreach($row in $Rows){
+        $psObjects += new-object PSCustomObject -property @{
+            id = $row.id
+            object = $row.object
+        }
+    }
+    $psObjects
+}
+
+function Convert-Object {
+    param([PSCustomobject]$object)
+
+        $Serialized = [System.Management.Automation.PSSerializer]::Serialize($object)
+    
+        $bytes = [System.Text.Encoding]::Unicode.GetBytes($Serialized)
+        [Convert]::ToBase64String($Bytes)
+}
+function Convert-ToObject {
+    param($string)
+    
+        $obj = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($string))
+    
+        [System.Management.Automation.PSSerializer]::Deserialize($obj)
+    
+}
+
+function PrepareDB {
+
+    $connection = New-DBConnection
+
+    Invoke-DBNonQuery -connection $connection -cmd "CREATE TABLE OBJECTS (id TEXT PRIMARY KEY, object TEXT)"
+    
+
+    Close-DBConnection -Connection $connection
+}
+
+function New-PersistentObject {
 <#
  .Synopsis
   Converts an object to a base64 string and saves it to a local SQLite database
@@ -10,45 +193,33 @@ function New-PSPersistentObject {
   The object that should be saved in persistent storage
 
  .Parameter id
-  Identifier for the object, if not specified a new guid will be generated and used as the identifier
+  # Identifier for the object, if not specified a new guid will be generated and used as the identifier
  
- .Parameter datasource
-  Specifies the path of a SQLite database file. If not used a "PSPersistentObject.db" will be used
-
  .Example
    # Creates a new persistent object and stores it in the default PSPersistentObject.db
    New-PersistentObject -Object $myObject -Id 1 
 
- .Example
-   # Creates a new persistent object and stores it in .\my_db.db
-   New-PersistentObject -Object $myObject -Id 1 -datasource .\my_db.db
+
 #>
  [CmdletBinding()]
  param(
-     [Parameter(Position=0,
+    [Parameter(Position=0,
+               Mandatory = $false,
+               ValueFromPipeline = $false)]
+               [String]$Id,
+
+     [Parameter(Position=1,
                 Mandatory = $true,
                 ValueFromPipeline=$true,
                 HelpMessage="Object to save required...")]
                 [Object]$Object,
 
-    [Parameter(Position=1,
-               Mandatory = $false,
-               ValueFromPipeline = $false)]
-               [String]$Id,
-
-    [Parameter(Position=2,
-               Mandatory = $false,
-               ValueFromPipeline=$false)]
-               [String]$DataSource        
- )
+                [Switch]$Force
+  )
 
  Begin {
 
-    try {
-        # Import sqlite assemblies
-    } catch {
-        Throw 'This module requires an SQLite driver'
-    }
+
 
     if(-not $Id){
         # Id parameter was not specified. Using guid as the identifier
@@ -56,18 +227,53 @@ function New-PSPersistentObject {
         $Id = New-Guid | Select-Object -ExpandProperty Guid
 
     }
+    
+    $Test = Get-PersistentObject -Id $id
 
+    $connection = New-DBConnection
+
+    if($Test){
+        Write-Verbose "Id $id already in use"
+
+        if($Force){
+
+            Invoke-DBNonQuery -connection $connection -query "delete from objects where id = '$id'"
+        
+        } else {
+
+        
+
+            Write-Error "Id $id already in use and the -Force switch was not used"
+            break
+        }
+
+
+    }
 
 
  }
 
  Process {
 
-    Write-Output $id
 
- }
+    
+
+    $Base64 = Convert-Object -object $Object
+
+   
+
+
+ 
+    Invoke-DBNonQuery -connection $connection -query "Insert into objects (id, object) values ('$id', '$Base64')"
+
+
+}
 
  End {
+
+    Close-DBConnection -Connection $connection
+
+    Get-PersistentObject -Id $id
 
 
  }
@@ -76,3 +282,70 @@ function New-PSPersistentObject {
 
 }
 
+function Get-PersistentObject {
+    <#
+ .Synopsis
+  Get a previously saved object from the database
+
+ .Description
+  Get a previously saved object from the database
+
+ .Parameter id
+  Identifier for the object, if not specified all objects will be returned
+ 
+ .Example
+   # Get the object with Id 1
+   Get-PersistentObject -Id 1 
+
+#>
+    param(
+        [Parameter(Position=0,
+                   Mandatory = $false,
+                   ValueFromPipeline = $false)]
+                   [String]$Id
+    )
+
+    Begin {
+
+
+
+        if(-not $Id){
+            # Id parameter was not specified. Getting all objects
+            $Query = "Select id, object from objects"
+        } else {
+            $Query = "Select id, object from objects where id = '$id'"
+        } 
+    
+    
+     }
+    
+     Process {
+    
+      
+        
+        $connection = New-DBConnection
+        $object = Invoke-DBQuery -connection $connection -query "$Query"
+    
+  
+    }
+    
+     End {
+    
+        Close-DBConnection $connection
+        $obj = @()
+        foreach($o in $object){
+        $obj += Convert-ToObject -String $o.object
+        }
+        $obj
+    
+     }
+ 
+}
+
+
+PrepareDB
+
+#Export-ModuleMember -Function New-PersistentObject
+#Export-ModuleMember -Function Save-PersistentObject
+#Export-ModuleMember -Function Get-PersistentObject
+#Export-ModuleMember -Function Remove-PersistentObject
